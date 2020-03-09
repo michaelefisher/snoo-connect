9c45e8c73e92c21092195c529f7e471e6c09b2cc
Michael E Fisher <fisher@corybooker.com>
2020-03-08 22:23:38 -0400

    refactor

--- api/main.py
+++ api/main.py
@@ -3,6 +3,12 @@ import datetime
 import os
 import requests
 import pytz
+import hashlib
+import logging
+
+logger = logging.Logger('Main', 1)
+
+logger.setLevel(logging.INFO)
 
 local_tz = pytz.timezone('America/New_York')
 
@@ -29,6 +35,46 @@ def convert(seconds):
     hour, min = divmod(minutes, 60)
     return "%dh%dm" % (hour, min)
 
+def is_sleeping(session_dict):
+    now = datetime.datetime.now()
+    if session_dict['end_time'] > now.strftime('%Y-%m-%dT%H:%M:%S'):
+        print('We\'re still sleeping.')
+
+def send_data(session_params):
+    with requests.Session() as s:
+        params = {'email': BABY_CONNECT_USERNAME,
+                   'pass': BABY_CONNECT_PASSWORD
+                 }
+        s.post(
+            BABY_CONNECT_AUTH_URL,
+            data=params,
+            headers = {'content-type': 'application/x-www-form-urlencoded'})
+
+        request_cookies = s.cookies
+        seacloud_cookie = request_cookies.get('seacloud1')
+
+        params = {'C': 500,
+                  'Kid': BABY_CONNECT_KID_ID,
+                  'uts': session_params['start_time'].astimezone(local_tz).strftime('%H%M'),
+                  'ptm': session_params['end_time'].astimezone(local_tz).strftime('%H%M'),
+                  'pdt': session_params['current_date'].astimezone(local_tz).strftime('%y%m%d'),
+                  'fmt': 'long',
+                  'd': round(session_params['duration'] / 60),
+                  'tz': -4,
+                  'listKid': -1,
+                  'txt': session_params['text_string'],
+                  'e': session_params['end_string'],
+                 }
+
+        response = s.post(BABY_CONNECT_DATA_POST_URL,
+                        data=params,
+                        headers = {'content-type':
+                                   'application/x-www-form-urlencoded',
+                                  },
+                          cookies={'seacloud1': seacloud_cookie})
+
+        logger.info(response)
+
 def main():
     client = snoo.Client()
 
@@ -44,15 +90,17 @@ def main():
     else:
         session = client.get_current_session()
         sessions_list.append(session)
-
     for day in sessions_list:
         for session in day.sessions:
+            print(day.sessions)
             session = session.to_dict()
-            if session['end_time'] and session['start_time']:
+            is_sleeping(session)
+            if session['start_time']:
                 try:
                     start_time = datetime.datetime.strptime(session['start_time'],
                                                             '%Y-%m-%dT%H:%M:%S').replace(tzinfo=local_tz)
                     end_time = datetime.datetime.strptime(session['end_time'],'%Y-%m-%dT%H:%M:%S').replace(tzinfo=local_tz)
+
                     current_date = datetime.datetime.strptime(session['start_time'],
                                                             '%Y-%m-%dT%H:%M:%S').replace(tzinfo=local_tz)
                     duration = session['duration']
@@ -61,47 +109,22 @@ def main():
                     end_string = datetime.datetime.strftime(end_time.astimezone(local_tz),
                                                             '%-m/%d/%Y %H:%M')
 
-                    print(f'start time: {start_time}, end time: {end_time}')
-                    print(f'current_date: {current_date}, duration: {convert(duration)}')
+                    logger.info(f'start time: {start_time}, end time: {end_time}')
+                    logger.info(f'current_date: {current_date}, duration: {convert(duration)}')
                 except ValueError as e:
-                    print('Error converting time: %s' % (e))
+                    logger.info('Error converting time: %s' % (e))
 
-                with requests.Session() as s:
-                    params = {'email': BABY_CONNECT_USERNAME,
-                               'pass': BABY_CONNECT_PASSWORD
-                             }
-                    s.post(
-                        BABY_CONNECT_AUTH_URL,
-                        data=params,
-                        headers = {'content-type': 'application/x-www-form-urlencoded'})
+                session_params = {}
+                session_params['start_time'] = start_time
+                session_params['end_time'] = end_time
+                session_params['duration'] = duration
+                session_params['current_date'] = current_date
+                session_params['text_string'] = text_string
+                session_params['end_string'] = end_string
 
-                    request_cookies = s.cookies
-                    seacloud_cookie = request_cookies.get('seacloud1')
-
-                    params = {'C': 500,
-                              'Kid': BABY_CONNECT_KID_ID,
-                              'uts': start_time.astimezone(local_tz).strftime('%H%M'),
-                              'ptm': end_time.astimezone(local_tz).strftime('%H%M'),
-                              'pdt': current_date.astimezone(local_tz).strftime('%y%m%d'),
-                              'fmt': 'long',
-                              'd': round(duration / 60),
-                              'tz': -4,
-                              'listKid': -1,
-                              'txt': text_string,
-                              'e': end_string,
-                             }
-
-                    print(params)
-
-                    response = s.post(BABY_CONNECT_DATA_POST_URL,
-                                    data=params,
-                                    headers = {'content-type':
-                                               'application/x-www-form-urlencoded',
-                                              },
-                                      cookies={'seacloud1': seacloud_cookie})
-
-                    print(response.text)
+                send_data(session_params)
 
 if __name__ == "__main__":
     main()
+
 
